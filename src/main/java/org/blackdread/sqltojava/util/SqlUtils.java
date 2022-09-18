7e5f3276b3381fde9867f767509b85fd55a929fe
jason.long <jason@octgsoftware.com>
2022-09-18 18:10:16 -0500

    Allow space in data type so character varying(25) and others will work.

--- src/main/java/org/blackdread/sqltojava/util/SqlUtils.java
+++ src/main/java/org/blackdread/sqltojava/util/SqlUtils.java
@@ -23,7 +23,7 @@ import org.slf4j.LoggerFactory;
 public final class SqlUtils {
     private static final Logger log = LoggerFactory.getLogger(SqlUtils.class);
 
-    private static final Pattern COLUMN_TYPE_SIZE_REGEX = Pattern.compile("(^[a-z]+)\\(([0-9]+)\\)$", Pattern.CASE_INSENSITIVE);
+    private static final Pattern COLUMN_TYPE_SIZE_REGEX = Pattern.compile("(^[a-z\\s]+)\\(([0-9]+)\\)$", Pattern.CASE_INSENSITIVE);
 
     private SqlUtils() {}
 
