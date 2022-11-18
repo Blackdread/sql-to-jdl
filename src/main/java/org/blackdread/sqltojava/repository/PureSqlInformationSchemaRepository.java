d3a736dcd709f40a2583feeea1cc662b705bcf27
jason.long <jason@octgsoftware.com>
2022-11-18 09:49:33 -0600

    Add PureSqlInformationSchemaRepository and make sql files load based on profile.  This will eliminate needing separate repositions for each database.

--- src/main/java/org/blackdread/sqltojava/repository/PureSqlInformationSchemaRepository.java
+++ src/main/java/org/blackdread/sqltojava/repository/PureSqlInformationSchemaRepository.java
@@ -13,27 +13,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Profile("postgresql")
-public class PostgresInformationSchemaRepository implements InformationSchemaRepository {
+@Profile({ "postgresql" })
+public class PureSqlInformationSchemaRepository implements InformationSchemaRepository {
 
-    private static final Logger log = LoggerFactory.getLogger(PostgresInformationSchemaRepository.class);
-    public static final String ALL_TABLE_RELATIONAL_INFROMATION = ResourceUtil.readString(
-        "sql/postgres-getAllTableRelationInformation.sql"
-    );
-    public static final String FULL_COLUMN_INFORMATION_OF_TABLE = ResourceUtil.readString(
-        "sql/postgres-getFullColumnInformationOfTable.sql"
-    );
-    public static final String ALL_TABLE_INFORMATION = ResourceUtil.readString("sql/postgres-getAllTableInformation.sql");
+    private static final Logger log = LoggerFactory.getLogger(PureSqlInformationSchemaRepository.class);
+    private static String ALL_TABLE_RELATIONAL_INFROMATION;
+    private static String FULL_COLUMN_INFORMATION_OF_TABLE;
+    private static String ALL_TABLE_INFORMATION;
     private static final TableInformationRowMapper TABLE_INFORMATION_MAPPER = new TableInformationRowMapper();
-
     private final NamedParameterJdbcTemplate template;
 
     @Autowired
-    public PostgresInformationSchemaRepository(NamedParameterJdbcTemplate template) {
+    public PureSqlInformationSchemaRepository(NamedParameterJdbcTemplate template, ConfigurableEnvironment env) {
+        String activeProfile = env.getActiveProfiles()[0];
+        /**
+         *  TODO Handle mysql and mariadb needing the same sql files.  mysql|mariadb-getAllTableRelationInformation.sql
+         *  Find all files ending with getAllTableRelationInformation then filter for match.
+         */
+        ALL_TABLE_RELATIONAL_INFROMATION = readSqlFileByProfile(activeProfile, "getAllTableRelationInformation");
+        FULL_COLUMN_INFORMATION_OF_TABLE = readSqlFileByProfile(activeProfile, "getFullColumnInformationOfTable");
+        ALL_TABLE_INFORMATION = readSqlFileByProfile(activeProfile, "getAllTableInformation");
         this.template = template;
     }
 
@@ -52,5 +56,9 @@ public class PostgresInformationSchemaRepository implements InformationSchemaRep
     public List<TableInformation> getAllTableInformation(final String schemaName) {
         Map<String, ?> paramMap = Map.of("schemaName", schemaName);
         return template.queryForStream(ALL_TABLE_INFORMATION, paramMap, TABLE_INFORMATION_MAPPER).toList();
+    }
+
+    private static String readSqlFileByProfile(String activeProfile, String queryName) {
+        return ResourceUtil.readString(String.format("sql/%s-%s.sql", activeProfile, queryName));
     }
 }
