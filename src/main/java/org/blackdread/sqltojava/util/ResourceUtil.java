ede51f3ff8c4e682d97089b13c954aa4f19f200f
Emiliano Menéndez <emystein@gmail.com>
2024-10-06 19:08:49 -0300

    Fix load sql scripts from the classpath

--- src/main/java/org/blackdread/sqltojava/util/ResourceUtil.java
+++ src/main/java/org/blackdread/sqltojava/util/ResourceUtil.java
@@ -1,24 +1,23 @@
 package org.blackdread.sqltojava.util;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 public final class ResourceUtil {
 
     private static final Logger log = LoggerFactory.getLogger(ResourceUtil.class);
 
     public static String readString(Resource resource) {
-        String s;
-        try {
-            File file = resource.getFile();
-            s = new String(Files.readAllBytes(file.toPath()));
-            return s;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining(System.lineSeparator()));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
